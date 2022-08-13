e6f56dcd87e5fff0df810bb1f4724fc53a043808
wivl <68458084+wivl@users.noreply.github.com>
2022-08-13 23:47:04 +0800

    add comments

--- src/rdh.c
+++ src/rdh.c
@@ -3,6 +3,11 @@
 #include "rdh.h"
 #include "png.h"
 
+/*
+Generate key png file that stream encrypt process needed.
+width, height: The width and height of the image to be encrypted.
+The key png file is saved to current directory.
+ */
 void generate_key_image(const unsigned width, const unsigned height) {
 	unsigned char *image = (unsigned char *)malloc(width*height);
 
@@ -16,6 +21,12 @@ void generate_key_image(const unsigned width, const unsigned height) {
 
 }
 
+/*
+Load key from file. 
+filename: key file name.
+image: buffer that saved key file.
+width, height: get width and height of the key.
+ */
 void get_key_image(const char *filename, unsigned char **image,
 		unsigned *width, unsigned *height) {
 
@@ -26,8 +37,15 @@ void get_key_image(const char *filename, unsigned char **image,
 	decode_png(image, width, height, png, pngsize);
 }
 
+/*
+stream encryption. 
+ */
 void stream_encrypt(const unsigned char *key, unsigned char *image, const long bufsize) {
 	for (long i = 0; i < bufsize; i++) {
 		image[i] ^= key[i];
 	}
+}
+
+void watermark(unsigned char *image, unsigned L) {
+
 }
